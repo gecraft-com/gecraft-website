74a973297ec334f07db3ad26b57a0a417352e971
valyukhov <alexzed@bk.ru>
2024-09-11 21:42:24 +0300

    chore:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
 
+import { Button, ButtonGroup } from '@nextui-org/button'
+
 function App() {
-  return <div className="text-2xl">GeCraft</div>
+  return (
+    <>
+      <div className="text-2xl">GeCraft</div>
+      <ButtonGroup>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </>
+  )
 }
 
 export default App
