d1464b1ee4e5c5d348fa84680855148a54046904
valyukhov <alexzed@bk.ru>
2024-09-11 21:29:22 +0300

    chore: :heavy_plus_sign: added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return <div>GeCraft</div>;
+  return <div className="text-2xl">GeCraft</div>
 }
 
-export default App;
+export default App
