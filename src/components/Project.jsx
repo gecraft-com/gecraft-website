d76cc902c8fc011f2f70e5f597daecea6baf878f
Vitalii-250885 <provitaleks@gmail.com>
2024-10-18 10:34:31 +0300

    correct modal window and projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 function Project({ project, handleClick, color }) {
   return (
-    <div className="w-full md:min-w-[43.43vw] md:max-w-[43.43vw] lg:min-w-[33%] lg:max-w-[33%] 2xl:min-w-[33%] 2xl:max-w-[33%]">
+    <div className="w-full md:w-[43.43vw] lg:w-[29.8vw] 2xl:w-[25vw]">
       <button className="border-0" onClick={() => handleClick(project)}>
         <img src={project.img} alt={project.client} className="w-full rounded-md" />
         <div className="mt-[5.14vw] block md:mt-5 md:flex md:gap-x-10 lg:w-[30vw] 2xl:w-[22.95vw] 2xl:gap-x-5">
