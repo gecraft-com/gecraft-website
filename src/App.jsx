932ff30f6c40e618b1197ce3989f4b55f37f8115
Vitalii-250885 <provitaleks@gmail.com>
2025-02-11 16:25:13 +0200

    delete old files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import ContactUs from './components/ContactUs'
-import Cursor from './components/Cursor'
-import Header from './components/Header'
-import Services from './components/Services'
-import Work from './components/Work'
-
 function App() {
   return (
     <>
-      <Header />
-      <Services />
-      <Work />
-      <ContactUs />
-      {window.innerWidth > 1024 ? <Cursor /> : null}
+      <div>
+        <h1>GeCraft</h1>
+      </div>
     </>
   )
 }
