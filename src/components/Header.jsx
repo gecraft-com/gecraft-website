a3f00713c5ab99e2603c53bdd2986243d5042b6f
Vitalii-250885 <provitaleks@gmail.com>
2025-02-12 10:50:24 +0200

    Corrected icons logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Icons from './Icons'
 function Header() {
   return (
     <header className="fixed flex w-full justify-between bg-primary-100 p-4">
-      <Icons name="logo-small" width={88} height={22} />
+      <Icons name="logo" width={88} height={22} />
       <Icons name="menu" />
     </header>
   )
