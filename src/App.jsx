56e549cce6a27091092167ac65914174a226a74b
Vitalii-250885 <provitaleks@gmail.com>
2025-02-12 13:46:37 +0200

    Added hero text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <Layout>
       <Services />
-      <p>
+      <p className="mt-6 text-lg font-semibold leading-6">
         Tailored solutions: bespoke design and development for your business challenges
       </p>
     </Layout>
