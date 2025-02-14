b375b923dc70dcc8446e340bc961ac366285cc7b
Vitalii-250885 <provitaleks@gmail.com>
2025-02-14 19:01:19 +0200

    added services page

---
+++ src/components/ServiceItem.jsx
@@ -0,0 +1,38 @@
+function ServiceItem({ service, even }) {
+  const { icon, serviceName, list, paragraph, anchor } = service
+
+  return (
+    <div
+      id={anchor}
+      className={`max-w-[78.875rem] rounded-3xl bg-primary-100 px-4 py-3.5 sm:p-6 xl:flex xl:items-center xl:rounded-[4rem] xl:px-11 xl:py-8 ${even ? '' : 'ml-auto'}`}
+    >
+      <img
+        src={icon}
+        alt="logo"
+        className={`hidden w-[29rem] 2xl:w-[35rem] ${even ? '' : '-scale-x-100'} xl:block`}
+      />
+      <div className="w-11/12">
+        <div className="flex items-center">
+          <img
+            src={icon}
+            alt="logo"
+            className={`w-20 sm:w-32 ${even ? '' : '-scale-x-100'} xl:hidden`}
+          />
+          <h2 className="text-lg font-semibold sm:text-3xl xl:text-4xl">{serviceName}</h2>
+        </div>
+        <ul className="mt-3 list-disc pl-6 text-sm font-medium sm:mt-4 sm:text-lg xl:pl-8 xl:text-xl">
+          {list.map((item, index) => (
+            <li key={index} className="">
+              {item}
+            </li>
+          ))}
+        </ul>
+        {paragraph && (
+          <p className="mt-2 text-sm sm:mt-3 sm:text-lg xl:text-xl">{paragraph}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ServiceItem
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import PortfolioList from "../components/PortfolioList"
-import Services from "../components/Services"
+import PortfolioList from '../components/PortfolioList'
+import Services from '../components/Services'
 
 function HomePage() {
   return (
     <>
-      <Services />
+      <Services location="home" />
       <p className="mt-6 text-lg font-semibold leading-6 sm:mt-12 sm:text-3xl xl:mt-14 xl:text-5xl 2xl:mt-20">
         Tailored solutions: bespoke design <br className="hidden sm:block" /> and
         development for your business challenges
