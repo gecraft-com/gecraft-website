27d6decb46ed72c6502b2832f9cfe3ade6d378ed
Vitalii-250885 <provitaleks@gmail.com>
2025-02-17 11:40:13 +0200

    added button go to top

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -102,6 +102,72 @@ function Icons({ name, width, height }) {
           />
         </svg>
       )}
+      {name === 'go-to-top' && (
+        <svg
+          width="76"
+          height="76"
+          viewBox="0 0 76 76"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g filter="url(#filter0_d_404_4480)">
+            <rect x="14" y="10" width="48" height="48" rx="24" fill="black" />
+            <path
+              d="M44.0702 31.57L38.0002 25.5L31.9302 31.57"
+              stroke="white"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M38 42.4999V25.6699"
+              stroke="white"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_404_4480"
+              x="0"
+              y="0"
+              width="76"
+              height="76"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="4" />
+              <feGaussianBlur stdDeviation="7" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_404_4480"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_404_4480"
+                result="shape"
+              />
+            </filter>
+          </defs>
+        </svg>
+      )}
     </>
   )
 }
