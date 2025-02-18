75b6fd95f4dbe4b78f680084e13c31239ab01013
Vitalii-250885 <provitaleks@gmail.com>
2025-02-18 18:51:53 +0200

    Update GCButton.jsx

--- src/components/GCButton.jsx
+++ src/components/GCButton.jsx
@@ -10,15 +10,13 @@ function GCButton({
   const getButtonStyles = (view) => {
     switch (view) {
       case 'green-header':
-        return 'w-36 flex justify-center items-center bg-primary-500 h-10 rounded-full text-white xl:w-44 xl:h-12'
+        return 'w-36 flex justify-center items-center bg-primary-500 h-10 rounded-full text-white xl:w-44 xl:h-12 hover:bg-primary-700 duration-100'
       case 'green-form':
         return 'w-full bg-primary-500 h-12 rounded-full text-white xl:text-lg'
       case 'cookies-first':
         return 'w-full bg-primary-500 h-12 rounded-full px-4 text-white xl:text-lg whitespace-nowrap hover:bg-primary-700 duration-100'
       case 'cookies-second':
         return 'w-full border border-primary-500 h-12 px-4 rounded-full text-primary-500 whitespace-nowrap hover:bg-primary-700 duration-100 hover:text-white hover:border-primary-700'
-      case 'menu':
-        return ''
       default:
         return ''
     }
