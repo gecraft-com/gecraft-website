ff6a1199e26e530f162d418c03df79ff65938677
Vitalii-250885 <provitaleks@gmail.com>
2025-02-24 19:25:20 +0200

    changed active and hover effects for buttons

--- src/components/GCButton.jsx
+++ src/components/GCButton.jsx
@@ -10,13 +10,13 @@ function GCButton({
   const getButtonStyles = (view) => {
     switch (view) {
       case 'green-header':
-        return 'w-36 flex justify-center items-center bg-primary-500 h-10 rounded-full text-white xl:w-44 xl:h-12 hover:bg-primary-700 duration-100'
+        return 'w-36 flex justify-center items-center bg-primary-500 h-10 rounded-full text-white xl:w-44 xl:h-12 hover:bg-primary-800 duration-100 active:bg-primary-700'
       case 'green-form':
-        return 'w-full bg-primary-500 h-12 rounded-full text-white xl:text-lg hover:bg-primary-700 duration-100'
+        return 'w-full bg-primary-500 h-12 rounded-full text-white xl:text-lg hover:bg-primary-800 duration-100 active:bg-primary-700'
       case 'cookies-first':
-        return 'w-full bg-primary-500 h-12 rounded-full px-4 text-white xl:text-lg whitespace-nowrap hover:bg-primary-700 duration-100'
+        return 'w-full bg-primary-500 h-12 rounded-full px-4 text-white xl:text-lg whitespace-nowrap hover:bg-primary-800 duration-100 active:bg-primary-700'
       case 'cookies-second':
-        return 'w-full border border-primary-500 h-12 px-4 rounded-full text-primary-500 whitespace-nowrap hover:bg-primary-700 duration-100 hover:text-white hover:border-primary-700'
+        return 'w-full border border-primary-500 h-12 px-4 rounded-full text-primary-500 whitespace-nowrap hover:bg-primary-800 duration-100 hover:text-white hover:border-primary-800 active:bg-primary-700'
       default:
         return ''
     }
