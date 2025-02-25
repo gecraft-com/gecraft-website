42a69d7be543036ef7ccb5bc6e0558899a603630
Vitalii-250885 <provitaleks@gmail.com>
2025-02-25 23:33:52 +0200

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -10,30 +10,7 @@ function ContactUs() {
         <h3 className="animation-timeline ml-[2.84vw] animate-emergence whitespace-nowrap text-left text-[3.08vw] font-extrabold uppercase before:mr-[1.3vw] before:inline-flex before:h-[2.31vw] before:w-[2.31vw] before:rounded-full before:bg-basic sm:ml-0 sm:text-[1.06vw] sm:before:mr-[0.45vw] sm:before:h-[0.8vw] sm:before:w-[0.8vw] lg:text-[0.63vw] lg:before:mr-[0.27vw] lg:before:h-[0.47vw] lg:before:w-[0.47vw] 2xl:text-[0.35vw] 2xl:before:mr-[0.15vw] 2xl:before:h-[0.263vw] 2xl:before:w-[0.263vw]">
           Contact us
         </h3>
-        <p className="animation-timeline mt-[7.7vw] animate-emergence text-[5.15vw] font-bold uppercase sm:mt-[2.65vw] sm:text-[1.77vw] lg:mt-[1.57vw] lg:text-[1.05vw] 2xl:mt-[1.46vw] 2xl:text-[0.59vw]">
-          Phone
-        </p>
-        <a
-          href="tel:+995558599550"
-          className="animation-timeline mt-[1.3vw] animate-emergence text-[6.7vw] font-bold sm:mt-[0.45vw] sm:text-[2.3vw] lg:mt-[0.27vw] lg:text-[1.36vw] 2xl:mt-[0.15vw] 2xl:text-[1.4vw]"
-        >
-          +(995) 558 599 550
-        </a>
-        <a
-          href="tel:+16475940256"
-          className="animation-timeline mt-[1.3vw] animate-emergence text-[6.7vw] font-bold sm:mt-[0.45vw] sm:text-[2.3vw] lg:mt-[0.27vw] lg:text-[1.36vw] 2xl:mt-[0.15vw] 2xl:text-[1.4vw]"
-        >
-          +1 (647) 594 0256
-        </a>
-        <p className="animation-timeline mt-[5.15vw] animate-emergence text-[5.15vw] font-bold uppercase sm:mt-[1.77vw] sm:text-[1.77vw] lg:mt-[1.05vw] lg:text-[1.05vw] 2xl:mt-[0.59vw] 2xl:text-[0.59vw]">
-          Email
-        </p>
-        <a
-          href="mailto:info@gecraft.com"
-          className="animation-timeline mt-[1.3vw] animate-emergence text-[6.7vw] font-bold sm:mt-[0.45vw] sm:text-[2.3vw] lg:mt-[0.27vw] lg:text-[1.36vw] 2xl:mt-[0.15vw] 2xl:text-[1.4vw]"
-        >
-          info@gecraft.com
-        </a>
+
         <div className="mt-[5.15vw] flex items-center sm:mt-[3.53vw] lg:mt-[2.09vw] 2xl:mt-[1.17vw]">
           <a href="https://gecraft.com/">
             <img
