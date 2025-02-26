8bfe549acb5017da0743bce1d16425e017e03e14
Vitalii-250885 <provitaleks@gmail.com>
2025-02-26 18:39:07 +0200

    Exported icons with x2

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -18,11 +18,11 @@ function ServiceItem({ service, odd }) {
         src={icon}
         alt="logo"
         loading="lazy"
-        className="hidden w-[29rem] xl:block 2xl:w-[35rem]"
+        className="hidden xl:mr-8 xl:block xl:w-80"
       />
       <div className="w-11/12">
-        <div className="flex items-center">
-          <img src={icon} alt="logo" loading="lazy" className="w-20 sm:w-32 xl:hidden" />
+        <div className="flex items-center gap-4">
+          <img src={icon} alt="logo" loading="lazy" className="w-16 sm:w-24 xl:hidden" />
           <h2 className="text-lg font-semibold sm:text-3xl xl:text-4xl">{serviceName}</h2>
         </div>
         <ul className="mt-3 list-disc pl-6 text-sm font-medium sm:mt-4 sm:text-lg xl:pl-8 xl:text-xl">
