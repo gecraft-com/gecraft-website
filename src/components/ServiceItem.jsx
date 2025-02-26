7c5f1e58ad02f82bf84c13db3f0c08062a5a1205
Vitalii-250885 <provitaleks@gmail.com>
2025-02-26 10:46:11 +0200

    Delete flip for service icons

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,25 +1,28 @@
-function ServiceItem({ service, even }) {
+import clsx from 'clsx'
+
+function ServiceItem({ service, odd }) {
   const { icon, serviceName, list, paragraph, anchor } = service
 
   return (
     <div
       id={anchor}
-      className={`max-w-[78.875rem] rounded-3xl bg-primary-100 px-4 py-3.5 sm:p-6 xl:flex xl:items-center xl:rounded-[4rem] xl:px-11 xl:py-8 ${even ? '' : 'ml-auto'} scroll-mt-20 xl:scroll-mt-24`}
+      className={clsx(
+        'max-w-[78.875rem] scroll-mt-20 rounded-3xl bg-primary-100 px-4 py-3.5 sm:p-6 xl:flex xl:scroll-mt-24 xl:items-center xl:rounded-[4rem] xl:px-11 xl:py-8',
+        {
+          'mr-auto': odd,
+          'ml-auto': !odd,
+        }
+      )}
     >
       <img
         src={icon}
         alt="logo"
         loading="lazy"
-        className={`hidden w-[29rem] 2xl:w-[35rem] ${even ? '' : '-scale-x-100'} xl:block`}
+        className="hidden w-[29rem] xl:block 2xl:w-[35rem]"
       />
       <div className="w-11/12">
         <div className="flex items-center">
-          <img
-            src={icon}
-            alt="logo"
-            loading="lazy"
-            className={`w-20 sm:w-32 ${even ? '' : '-scale-x-100'} xl:hidden`}
-          />
+          <img src={icon} alt="logo" loading="lazy" className="w-20 sm:w-32 xl:hidden" />
           <h2 className="text-lg font-semibold sm:text-3xl xl:text-4xl">{serviceName}</h2>
         </div>
         <ul className="mt-3 list-disc pl-6 text-sm font-medium sm:mt-4 sm:text-lg xl:pl-8 xl:text-xl">
