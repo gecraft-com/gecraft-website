903994f9893d89c02fdbb3c789f32a225ac54f34
Vitalii-250885 <provitaleks@gmail.com>
2025-08-19 15:54:48 +0300

    fear: added font

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,10 @@ import { Services } from '../components/Services'
 export const HomePage = () => {
   return (
     <>
+      <p className="text-center font-[Faberge]">
+        <span className="">End-to-end digital services:</span> strategy, design, and
+        development for business growth
+      </p>
       <Services location="home" />
       <p className="font-readexPro mt-6 text-lg leading-6 font-semibold sm:mt-12 sm:text-3xl xl:mt-14 xl:text-4xl 2xl:mt-20">
         Tailored solutions: bespoke design <br className="hidden sm:block" /> and
