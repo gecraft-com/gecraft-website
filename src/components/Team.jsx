26552c1e4f2fdfd8a0bc144ca56bdaa6d2909cb4
Vitalii-250885 <provitaleks@gmail.com>
2025-08-20 14:05:30 +0300

    feat: tablet design have been changed

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,14 +4,14 @@ import { TeamItem } from './TeamItem'
 
 export const Team = () => {
   return (
-    <div className="mt-6 sm:mt-11 xl:mt-14 2xl:mt-20">
+    <div className="mt-6 md:mt-11 xl:mt-14 2xl:mt-20">
       <div className="relative mx-auto w-max">
-        <h3 className="text-center font-[Faberge] text-xl sm:text-3xl xl:text-5xl">
+        <h3 className="text-center font-[Faberge] text-xl md:text-3xl xl:text-5xl">
           Our team
         </h3>
-        <Lines3 className="absolute -bottom-2.5 -left-2 w-25" />
+        <Lines3 className="absolute -bottom-2.5 -left-2 w-25 md:-bottom-4.5 md:-left-3 md:w-38" />
       </div>
-      <ul className="mt-4 grid grid-cols-2 gap-2 min-[950px]:grid-cols-4 min-[1440px]:grid-cols-5 min-[1750px]:gap-12 sm:mt-6 sm:grid-cols-3 xl:mt-9 xl:gap-7 2xl:gap-7">
+      <ul className="min-237.5:grid-cols-4 min-360:grid-cols-5 min-437.5:gap-12 mt-4 grid grid-cols-2 gap-2 md:mt-6 md:grid-cols-3 md:gap-4 xl:mt-9 xl:gap-7 2xl:gap-7">
         {team.map((person) => (
           <TeamItem key={person.id} person={person} />
         ))}
--- src/components/TeamItem.jsx
+++ src/components/TeamItem.jsx
@@ -2,16 +2,16 @@ export const TeamItem = ({ person }) => {
   const { photo, name, role } = person
 
   return (
-    <div className="group bg-black-00 hover:bg-primary-650 rounded-2xl px-3.5 py-3 duration-100 sm:px-4 sm:py-3.5 xl:rounded-3xl xl:p-5">
+    <div className="group bg-black-00 hover:bg-primary-650 rounded-2xl px-3.5 py-3 duration-100 md:px-4 md:py-3.5 xl:rounded-3xl xl:p-5">
       <img
         src={photo}
         alt={name}
         className="bg-black-50 mx-auto h-auto w-auto rounded-lg xl:rounded-2xl"
       />
-      <p className="text-black-950 mt-2 font-semibold duration-100 group-hover:text-white sm:text-xl xl:mt-4 xl:text-3xl">
+      <p className="text-black-950 mt-2 font-semibold duration-100 group-hover:text-white md:text-xl xl:mt-4 xl:text-3xl">
         {name}
       </p>
-      <p className="text-black-900 mt-0.5 text-sm duration-100 group-hover:text-white sm:mt-2 sm:text-base xl:text-lg">
+      <p className="text-black-900 mt-0.5 text-sm duration-100 group-hover:text-white md:mt-2 md:text-base xl:text-lg">
         {role}
       </p>
     </div>
